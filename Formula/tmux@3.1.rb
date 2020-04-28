9315b5fe24d03edde7a254b6bdd11ee36d2c760b
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2020-04-28 18:32:17 +0900

    Update Formula/{tmux,tmux@3.1}.rb

--- Formula/tmux@3.1.rb
+++ Formula/tmux@3.1.rb
@@ -1,38 +1,19 @@
-class Tmux < Formula
+class TmuxAT31 < Formula
   desc "Terminal multiplexer"
   homepage "https://tmux.github.io/"
-  revision 4
 
-  stable do
-    tmux_version = "3.1"
-    url "https://github.com/tmux/tmux/releases/download/#{tmux_version}/tmux-#{tmux_version}.tar.gz"
-    sha256 "979bf38db2c36193de49149aaea5c540d18e01ccc27cf76e2aff5606bd186722"
-    version tmux_version
+  tmux_version = "3.1"
+  url "https://github.com/tmux/tmux/releases/download/#{tmux_version}/tmux-#{tmux_version}.tar.gz"
+  sha256 "979bf38db2c36193de49149aaea5c540d18e01ccc27cf76e2aff5606bd186722"
+  version tmux_version
 
-    patch do
-      url "https://github.com/z80oolong/tmux-eaw-fix/raw/master/tmux-#{tmux_version}-fix.diff"
-      sha256 "f9efcbdcd7048b549141ca06be435dbc142d99fefc06464995aea650f778d480"
-    end
-  end
+  keg_only :versioned_formula
 
-  head do
-    url "https://github.com/tmux/tmux.git"
-
-    patch do
-      url "https://raw.githubusercontent.com/z80oolong/tmux-eaw-fix/master/tmux-HEAD-79b4d839-fix.diff"
-      sha256 "80170b9962f10c768f5ee099f9d3bf10b09effeda34849e9bc127276885ca61b"
-    end
-
-    depends_on "automake" => :build
-    depends_on "autoconf" => :build
-    depends_on "bison" => :build
-  end
-
+  depends_on "pkg-config" => :build
   depends_on "z80oolong/tmux/tmux-libevent@2.2"
   depends_on "utf8proc" => :optional
   depends_on "ncurses" unless OS.mac?
 
-  option "with-version-master", "In head build, set the version of tmux as `master`."
   option "without-utf8-cjk", "Build without using East asian Ambiguous Width Character in tmux."
   option "without-pane-border-acs-ascii", "Build without using ACS ASCII as pane border in tmux."
 
@@ -41,21 +22,18 @@ class Tmux < Formula
     sha256 "05e79fc1ecb27637dc9d6a52c315b8f207cf010cdcee9928805525076c9020ae"
   end
 
+  patch do
+    url "https://github.com/z80oolong/tmux-eaw-fix/raw/master/tmux-#{tmux_version}-fix.diff"
+    sha256 "f9efcbdcd7048b549141ca06be435dbc142d99fefc06464995aea650f778d480"
+  end
+
   def install
     ENV.append "CFLAGS",   "-I#{Formula["z80oolong/tmux/tmux-libevent@2.2"].opt_include}"
     ENV.append "CPPFLAGS", "-I#{Formula["z80oolong/tmux/tmux-libevent@2.2"].opt_include}"
     ENV.append "LDFLAGS",  "-L#{Formula["z80oolong/tmux/tmux-libevent@2.2"].opt_lib}"
 
-    if build.head? && build.with?("version-master") then
-      inreplace "configure.ac" do |s|
-        s.gsub!(/AC_INIT\(\[tmux\],[^)]*\)/, "AC_INIT([tmux], master)")
-      end
-    end
-
     ENV.append "CPPFLAGS", "-DNO_USE_UTF8CJK" if build.without?("utf8-cjk")
     ENV.append "CPPFLAGS", "-DNO_USE_PANE_BORDER_ACS_ASCII" if build.without?("pane-border-acs-ascii")
-
-    system "sh", "autogen.sh" if build.head?
 
     args = %W[
       --disable-Dependency-tracking
