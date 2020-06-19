1a625e9abc43652a8ee09fa52413d9957c60cf18
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2020-06-19 15:46:42 +0900

    Fix Formula/tmux-ncurses@6.2.rb

--- Formula/tmux-ncurses@6.2.rb
+++ Formula/tmux-ncurses@6.2.rb
@@ -5,6 +5,7 @@ class TmuxNcursesAT62 < Formula
   mirror "https://ftpmirror.gnu.org/ncurses/ncurses-6.2.tar.gz"
   sha256 "30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
   version "6.2"
+  revision 1
 
   keg_only :versioned_formula
 
@@ -17,6 +18,8 @@ class TmuxNcursesAT62 < Formula
   def install
     system "./configure", "--prefix=#{prefix}",
                           "--enable-pc-files",
+                          "--with-terminfo-dirs=#{opt_share}/terminfo:#{share}/terminfo",
+                          "--with-default-terminfo-dir=#{share}/terminfo",
                           "--with-pkg-config-libdir=#{lib}/pkgconfig",
                           "--enable-sigwinch",
                           "--enable-symlinks",
