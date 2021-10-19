b8ec85edf1b745d7c8d8ab8ae2c221d9f48fc109
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2021-10-19 17:38:12 +0900

    Fix Formula/tmux-ncurses@6.2

--- Formula/tmux-ncurses@6.2.rb
+++ Formula/tmux-ncurses@6.2.rb
@@ -1,7 +1,8 @@
 class TmuxNcursesAT62 < Formula
   desc "Text-based UI library for tmux."
   homepage "https://www.gnu.org/software/ncurses/"
-  url "https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz"
+  url "https://invisible-mirror.net/archives/ncurses/ncurses-6.2.tar.gz"
+  mirror "https://ftp.gnu.org/gnu/ncurses/ncurses-6.2.tar.gz"
   mirror "https://ftpmirror.gnu.org/ncurses/ncurses-6.2.tar.gz"
   sha256 "30306e0c76e0f9f1f0de987cf1c82a5c21e1ce6568b9227f7da5b71cbea86c9d"
   version "6.2"
