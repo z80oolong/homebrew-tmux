ea7fd6faad7d56b397e7ee4beb44ba89e96e1352
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2022-03-12 03:00:36 +0900

    Fix Formula/tmux@*.rb

--- Formula/tmux@3.2.rb
+++ Formula/tmux@3.2.rb
@@ -83,31 +83,6 @@ class TmuxAT32 < Formula
     EOS
   end
 
-  # For brew appimage-build
-  def apprun; <<~EOS
-    #!/bin/sh
-    #export APPDIR="/tmp/.mount-tmuxXXXXXX"
-    if [ "x${APPDIR}" = "x" ]; then
-      export APPDIR="$(dirname "$(readlink -f "${0}")")"
-    fi
-    export PATH="${APPDIR}/usr/bin/:${PATH:+:$PATH}"
-    export LD_LIBRARY_PATH="${APPDIR}/usr/lib/:${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
-    export XDG_DATA_DIRS="${APPDIR}/usr/share/${XDG_DATA_DIRS:+:$XDG_DATA_DIRS}"
-    export TERMINFO="${APPDIR}/usr/share/terminfo"
-    unset ARGV0
-
-    exec "tmux" "$@"
-    EOS
-  end
-
-  def exec_path_list
-    [opt_bin/"tmux"]
-  end
-
-  def pre_build_appimage(appdirpath, verbose)
-    system("cp -pRv #{Formula["z80oolong/tmux/tmux-ncurses@6.2"].opt_share}/terminfo #{appdirpath}/usr/share")
-  end
-
   test do
     system "#{bin}/tmux", "-V"
   end
